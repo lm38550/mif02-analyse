b4d81d3d4da504b2ef6057f2a477e16d72e176ec
Oscar <oscar.gibert@etu.univ-lyon1.fr>
2026-01-12 16:04:57 +0100

    main update affichage histo

--- main.cpp
+++ main.cpp
@@ -17,8 +17,32 @@ void histogramme(cv::Mat& img, int* hist) {
     }
 }
 
+cv::Mat afficherHistogramme(int* hist) {
+    int histSize = 256;
+    int histH = 200;
+    int histW = histSize;
+
+    // Image blanche
+    cv::Mat histImage(histH, histW, CV_8UC1, cv::Scalar(255));
+
+    // Trouver la valeur max pour normalisation
+    int maxVal = 0;
+    for(int i = 0; i < histSize; i++)
+        if(hist[i] > maxVal) maxVal = hist[i];
+
+    // Dessiner les barres (noir, inversé)
+    for(int i = 0; i < histSize; i++) {
+        int barHeight = (int)(((float)hist[i] / maxVal) * histH);
+        for(int y = histH - barHeight; y < histH; y++) {  // inversé
+            histImage.at<uchar>(y, i) = 0;  // noir
+        }
+    }
+
+    return histImage;
+}
+
 int main() {
-    cv::Mat img = cv::imread("./test.jpg");
+    cv::Mat img = cv::imread("./lena_gray.gif");
     if (img.empty()) return 1;
 
     int hist[256] = { 0 };
@@ -28,6 +52,10 @@ int main() {
     for (int y = 0; y < 256; ++y) {
         std::cout << hist[y] << std::endl;
     }
+
+    cv::Mat histImg = afficherHistogramme(hist);
+    cv::imshow("Histogramme", histImg);
+    cv::waitKey(0);
 
     cv::imshow("test", img);
     cv::waitKey(0);
