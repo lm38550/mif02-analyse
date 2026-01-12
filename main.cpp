f1e277df9e4660a4ee7f3c0f198c10c4075e9b06
Morel Louis <louis.morel@etu.univ-lyon1.fr>
2026-01-12 15:29:36 +0100

    creating, histogramm calcul func

--- main.cpp
+++ main.cpp
@@ -7,9 +7,28 @@
 #include <algorithm>
 #include <string>
 
+void histogramme(cv::Mat& img, int* hist) {
+
+    for (int y = 0; y < img.rows; ++y) {
+        for (int x = 0; x < img.cols; ++x) {
+            uchar value = img.at<uchar>(y, x);
+            hist[value]++;
+        }
+    }
+}
+
 int main() {
     cv::Mat img = cv::imread("./test.jpg");
     if (img.empty()) return 1;
+
+    int hist[256] = { 0 };
+
+    histogramme(img, hist);
+
+    for (int y = 0; y < 256; ++y) {
+        std::cout << hist[y] << std::endl;
+    }
+
     cv::imshow("test", img);
     cv::waitKey(0);
     return 0;
